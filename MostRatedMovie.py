e80e404ee10292ab930e30656ec24f271a911aed
Sylvain Leroux <sylvain@chicoree.fr>
2016-02-29 16:02:46 +0100

    Fixed member initialization

--- MostRatedMovie.py
+++ MostRatedMovie.py
@@ -3,19 +3,23 @@ from mrjob.job import MRJob
 from mrjob.step import MRStep
 
 class MRMostRatedMovie(MRJob):
+	def __init__(self, *args, **kwargs):
+		super(MRMostRatedMovie, self).__init__(*args, **kwargs)
+		self.last=(0,[])
+
 	def steps(self):
 			return [
 				MRStep(mapper=self.mapper, reducer=self.reducer),
-				MRStep(reducer=self.reducer_get_max, reducer_final=self.reducer_final)
+				MRStep(reducer=self.reducer_get_last, reducer_final=self.reducer_final)
 			]
 	def mapper(self, key, line):
 		userID, movieID, rating, timestamp = line.split('\t')
 		yield movieID, 1
 
 	def reducer(self, movieID, value):
-		yield sum(value), movieID
+		yield "{:6d}".format(sum(value)), movieID
 
-	def reducer_get_max(self, *args):
+	def reducer_get_last(self, *args):
 		self.last = args
 
 	def reducer_final(self):
