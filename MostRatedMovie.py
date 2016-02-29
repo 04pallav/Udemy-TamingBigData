9a3c0362253691f781b70d8e1095f06839348276
Sylvain Leroux <sylvain@chicoree.fr>
2016-02-29 22:14:20 +0100

    Added a workaround for mrjob 0.4.6 issue 1141
    See https://github.com/Yelp/mrjob/issues/1141

--- MostRatedMovie.py
+++ MostRatedMovie.py
@@ -1,8 +1,12 @@
 from mrjob.job import MRJob
-
 from mrjob.step import MRStep
 
+import mrjob.protocol
+
+
 class MRMostRatedMovie(MRJob):
+	INTERNAL_PROTOCOL = mrjob.protocol.PickleProtocol
+
 	def __init__(self, *args, **kwargs):
 		super(MRMostRatedMovie, self).__init__(*args, **kwargs)
 		self.last=(0,[])
@@ -10,7 +14,8 @@ class MRMostRatedMovie(MRJob):
 	def steps(self):
 			return [
 				MRStep(mapper=self.mapper, reducer=self.reducer),
-				MRStep(reducer=self.reducer_get_last, reducer_final=self.reducer_final)
+				MRStep(mapper=self.mapper_identity, # mrjob 0.4.6 bug : https://github.com/Yelp/mrjob/issues/1141
+					   reducer=self.reducer_get_last, reducer_final=self.reducer_final)
 			]
 	def mapper(self, key, line):
 		userID, movieID, rating, timestamp = line.split('\t')
@@ -21,6 +26,9 @@ class MRMostRatedMovie(MRJob):
 
 	def reducer_get_last(self, *args):
 		self.last = args
+
+	def mapper_identity(self, *args):
+		yield args
 
 	def reducer_final(self):
 		count, movieIDs = self.last
