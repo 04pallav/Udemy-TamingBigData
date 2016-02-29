a9aee5aac982d6cb8f9c92a1b0f4592f6ca5f3b9
Sylvain Leroux <sylvain@chicoree.fr>
2016-02-29 16:01:32 +0100

    Added +WordCounter.py

---
+++ WordCounter.py
@@ -0,0 +1,16 @@
+from mrjob.job import MRJob
+
+import re
+
+WORD_REGEXP = re.compile(r"[^\W\d_]+", re.UNICODE)
+
+class MRWordCounter(MRJob):
+	def mapper(self, key, line):
+		for word in WORD_REGEXP.findall(unicode(line, "UTF8").lower()):
+			yield word, 1
+
+	def reducer(self, word, count):
+		yield word, sum(count)
+
+if __name__ == "__main__":
+	MRWordCounter.run()
