246df92ffed81b132782eab5f391301e2a45cef6
Sylvain Leroux <sylvain@chicoree.fr>
2016-03-01 20:23:38 +0100

    Working version of the SuperheroDistance

--- SuperheroDistance.py
+++ SuperheroDistance.py
@@ -11,69 +11,61 @@ BLACK = 2
 
 INFINITY = 9999
 
-class MRSuperheroDistance(MRJob):
-	"""
-		Map-reduce implementation of the breadth first algorithm
-	"""
-	def __init__(self, *args, **kwargs):
-		super(MRSuperheroDistance, self).__init__(*args, **kwargs)
+class MRPreProcess(MRJob):
+    OUTPUT_PROTOCOL = mrjob.protocol.JSONValueProtocol
 
-	def configure_options(self):
-	    super(MRSuperheroDistance, self).configure_options()
-	    self.add_passthrough_option(
-	        '--target', default=HULK, type=int, help='Your target superhero ID')
+    def configure_options(self):
+        super(MRPreProcess, self).configure_options()
+        self.add_passthrough_option(
+            '--target', default=HULK, type=int, help='Your target superhero ID')
 
+    def mapper(self, key, line):
+        fields = [int(i) for i in line.split()]
+        heroID=fields[0]
 
-	def steps(self):
-			return [
-				MRStep(mapper=self.pre_process),
-				MRStep(mapper=self.mapper_bsf_iteration, reducer=self.reducer_bsf_iteration),
-			#	MRStep(mapper=self.mapper_identity, # mrjob 0.4.6 bug : https://github.com/Yelp/mrjob/issues/1141
-			#		   reducer=self.reducer_get_last, reducer_final=self.reducer_final)
-			]
+        if fields[0] == self.options.target:
+            yield heroID, dict(heroID=heroID, companions=fields[1:], color=GRAY, distance=0)
+        else:
+            yield heroID, dict(heroID=heroID, companions=fields[1:], color=WHITE, distance=INFINITY)
 
-	def pre_process(self, key, line):
-		fields = [int(i) for i in line.split()]
-		heroID=fields[0]
+class MRBFSStep(MRJob):
+    """
+        Map-reduce implementation of the breadth first algorithm
+    """
+    INPUT_PROTOCOL = mrjob.protocol.JSONValueProtocol
+    OUTPUT_PROTOCOL = mrjob.protocol.JSONValueProtocol
 
-		if fields[0] == self.options.target:
-			yield heroID, dict(heroID=heroID, companions=fields[1:], color=GRAY, distance=0)
-		else:
-			yield heroID, dict(heroID=heroID, companions=fields[1:], color=WHITE, distance=INFINITY)
+    def mapper(self, key, hero):
+        if hero['color'] == GRAY:
+            siblingDistance = dict(color=GRAY, distance=hero['distance']+1)
+            for companion in hero['companions']:
+                yield companion, siblingDistance
+                if companion == 10000:
+                    self.increment_counter("superhero","found")
+            hero['color'] = BLACK
+            self.increment_counter("superhero","touched")
 
-	def mapper_bsf_iteration(self, key, hero):
-		if hero['color'] == GRAY:
-			siblingDistance = dict(color=GRAY, distance=hero['distance']+1)
-			for companion in hero['companions']:
-				yield companion, siblingDistance
-			hero['color'] = BLACK
-
-		# In all cases emit the hero
-		yield hero['heroID'], hero
+        # In all cases emit the hero
+        yield hero['heroID'], hero
 
 
 
-	def reducer_bsf_iteration(self, heroID, records):
-		# Merge records
-		hero = dict(heroID=heroID, color=WHITE, distance=INFINITY, companions=[])
-		for record in records:
-			hero['companions'] += record.get('companions', [])
-			hero['color'] = max(hero['color'], record['color'])
-			hero['distance'] = min(hero['distance'], record['distance'])
+    def reducer(self, heroID, records):
+        # Merge records
+        hero = dict(heroID=heroID, color=WHITE, distance=INFINITY, companions=[])
+        for record in records:
+            hero['companions'] += record.get('companions', [])
+            hero['color'] = max(hero['color'], record['color'])
+            hero['distance'] = min(hero['distance'], record['distance'])
 
-		yield heroID, hero
-
-	def reducer_get_last(self, *args):
-		self.last = args
-
-	def mapper_identity(self, *args):
-		yield args
-
-	def reducer_final(self):
-		count, records = self.last
-		for record in records:
-			yield count, record
+        yield heroID, hero
 
 
-if __name__ == "__main__":
-	MRSuperheroDistance.run()
+class MRResult(MRJob):
+    INPUT_PROTOCOL = mrjob.protocol.JSONValueProtocol
+    OUTPUT_PROTOCOL = mrjob.protocol.RawValueProtocol
+
+    def mapper(self, key, hero):
+        if hero['color'] > WHITE:
+            yield key, dict(heroID=hero['heroID'], distance=hero['distance'], color=hero['color'])
+
--- SuperheroDistanceRunner.py
+++ SuperheroDistanceRunner.py
@@ -1,9 +1,35 @@
 # run.py
-from SuperheroDistance import MRSuperheroDistance
+from SuperheroDistance import MRPreProcess, MRBFSStep, MRResult
+from collections import defaultdict
 
-job = MRSuperheroDistance(args=["-v", "--strict-protocols", "../datasets/Marvel-Graph.txt"])
+dst = "d/step-0"
+job = MRPreProcess(args=["-v", "--strict-protocols", 
+                         "--output-dir=" + dst, "-"])
+with job.make_runner() as runner:
+    runner.run()
 
+done = False
+step = 0
+while not done:
+    src = dst
+    step += 1
+    dst = "d/step-{:d}".format(step)
+    job = MRBFSStep(args=["-v", "--strict-protocols", 
+                             "--output-dir=" + dst, src])
+    with job.make_runner() as runner:
+        runner.run()
+
+        counters = defaultdict(dict)
+        for counter in runner.counters():
+            counters.update(counter)
+        print counters
+        if 'found' in counters['superhero'] or 'touched' not in counters['superhero']:
+            done = True
+
+
+src = dst
+job = MRResult(args=[src])
 with job.make_runner() as runner:
     runner.run()
     for line in runner.stream_output():
-        print line
+        print line.strip()
